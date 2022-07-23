8b93bc40f4a59784fb987e574dd92d9ffb288fd0
danny188 <dyhh12@gmail.com>
2022-07-23 21:14:37 +1000

    Add loading bar when refreshing all products

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // add listener to delete notifications
     (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
       const $notification = $delete.parentNode;
 
@@ -6,4 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
         $notification.parentNode.removeChild($notification);
       });
     });
+
+    // display loading bar when refresh all products button clicked
+    if (document.querySelector('#refresh-all-products')) {
+        document.querySelector('#refresh-all-products').addEventListener('click', (event) => {
+            let numProducts = parseInt(document.querySelector('#num-of-products').textContent, 10);
+            let AVERAGE_API_RESPONSE_TIME_PER_PRODUCT = 0.8; // measured in seconds
+
+            document.querySelector('#updating-products-label').classList.remove('is-hidden');
+            document.querySelector('#updating-products-progress-bar').classList.remove('is-hidden');
+
+            document.querySelector('#update-all-products-approx-time').innerHTML = Math.round(numProducts * AVERAGE_API_RESPONSE_TIME_PER_PRODUCT);
+        });
+    }
   });
