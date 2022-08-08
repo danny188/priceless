44d191ba741bb813ee74322626182f36a86ac3b1
danny188 <dyhh12@gmail.com>
2022-08-08 18:08:41 +1000

    Add files for deployment to Heroku

--- priceless_project/settings.py
+++ priceless_project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import environ
 
-env = environ.Env()
+env = environ.Env(DEBUG=(bool, False))
 # reading .env file
 environ.Env.read_env()
 
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pricelessapp.herokuapp.com/']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
