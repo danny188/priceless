60ca04543433cbcb919f91e10d7f2bda2929c840
danny188 <dyhh12@gmail.com>
2022-08-19 14:05:02 +1000

    Remove incorrect reverse url reference for settings page

--- users/views.py
+++ users/views.py
@@ -20,7 +20,7 @@ def update_user_settings(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your settings are updated successfully', extra_tags="is-success is-light")
-            return redirect('update-settings')
+
     else:
         form = UpdateUserSettingsForm(instance=request.user)
 
