02e49c4a0903849fe7221f456a6a7bc5f2bace55
danny188 <dyhh12@gmail.com>
2022-08-21 12:15:43 +1000

    Allow users to delete their own account

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path
-from .views import RegisterView, update_user_settings
+from .views import RegisterView, delete_account_confirm_view, delete_account_view, update_user_settings
 from django.contrib.auth import views as auth_views
 
 from .forms import CustomAuthForm
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
+    path('delete_account_confirm', delete_account_confirm_view, name='delete-account-confirm'),
+    path('delete_account', delete_account_view, name='delete-account'),
     path('settings', update_user_settings, name='settings'),
     re_path(r'^login/$', auth_views.LoginView.as_view(authentication_form=CustomAuthForm), name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -25,3 +25,29 @@ def update_user_settings(request):
         form = UpdateUserSettingsForm(instance=request.user)
 
     return render(request, 'users/settings.html', {'form': form})
+
+
+@login_required
+def delete_account_confirm_view(request):
+    return render(request, "users/delete_account_confirm.html")
+
+
+@login_required
+def delete_account_view(request):
+    """Deletes a user account along with their product_set"""
+
+    if request.method == "POST":
+        confirm_account_email = request.POST.get("confirm-email").strip()
+
+        if request.user.email == confirm_account_email:
+            # delete linked products of user
+            request.user.product_set.all().delete()
+            # delete user
+            request.user.delete()
+
+            return redirect("/")
+        else:
+            messages.warning(request, 'The email address you entered did not match your account email', extra_tags="is-warning is-light")
+
+            return render(request, "users/delete_account_confirm.html")
+
